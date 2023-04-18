918d3c29fb40805e891d7a2664f96ea250426126
aNOOBis24 <mr.agay22@gmail.com>
2023-04-18 22:45:47 +0500

    hw fixes

--- project/bot/src/main/java/ru/tinkoff/edu/java/bot/dto/AddUserDto.java
+++ project/bot/src/main/java/ru/tinkoff/edu/java/bot/dto/AddUserDto.java
@@ -2,7 +2,7 @@ package ru.tinkoff.edu.java.bot.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record UserAddDto(@JsonProperty("username") String username,
+public record AddUserDto(@JsonProperty("username") String username,
                          @JsonProperty("first_name") String firstName,
                          @JsonProperty("last_name") String lastName) {
 }
